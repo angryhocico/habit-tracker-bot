64e59db92640bd60e76e7e6b57172af02ca29d36
angryhocico <dakkuringuvw@gmail.com>
2025-12-13 20:33:57 +0300

    Добавлена базовая структура проекта и зависимости

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+from handlers import router
+
+async def main():
+    load_dotenv()
+    token = os.getenv("BOT_TOKEN")
+    
+    logging.basicConfig(level=logging.INFO)
+
+    bot = Bot(token=token)
+    dp = Dispatcher()
+    
+    dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Бот выключен")
